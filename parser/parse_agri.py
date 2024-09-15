bb293794664846e4b7e9d0b5ecc722c761e6fae5
vinh412 <vinhduong412@gmail.com>
2024-09-15 22:11:06 +0700

    refactor

---
+++ parser/parse_agri.py
@@ -0,0 +1,5 @@
+import tabula
+import PyPDF2
+
+dics = tabula.read_pdf("agribank.pdf", pages=2, pandas_options={'header': None}, lattice=True, multiple_tables=False, output_format="json")
+print(dics)
